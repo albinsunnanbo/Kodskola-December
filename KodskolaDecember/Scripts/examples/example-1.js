25faf3a264059f1c0603aa78f5a20d3fc86c44bc
Albin Sunnanbo <albin@sunnanbo.com>
2014-12-16 13:27:54 +0100

    Added example with simple deffered execution

---
+++ KodskolaDecember/Scripts/examples/example-1.js
@@ -0,0 +1,9 @@
+﻿console.log("Print 1");
+$(function () {
+    console.log("Print 2");
+    setTimeout(function () {
+        console.log("Print 3");
+    }, 0);
+    console.log("Print 4");
+});
+console.log("Print 5");
