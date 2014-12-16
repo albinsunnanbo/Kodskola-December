f1f23254b83940073720c7491fcc4d7c0f3c6b32
Albin Sunnanbo <albin@sunnanbo.com>
2014-12-16 22:53:51 +0100

    Examples with closure

---
+++ KodskolaDecember/Scripts/examples/closure-example.js
@@ -0,0 +1,47 @@
+﻿$(function () {
+    $("#closure-example-1").click(function () {
+        var output = $("#output");
+        output.html(""); // clear
+
+        var i = 1;
+        var a = function () {
+            i++;
+        };
+        a();
+        output.append("<p>" + i + "</p>");
+    });
+
+
+    var b = function (x) { // x is called by value
+        // i is undefined here
+        x++;
+    };
+
+    $("#closure-example-2").click(function () {
+        var output = $("#output");
+        output.html(""); // clear
+
+        var i = 1;
+        b(i);
+        output.append("<p>" + i + "</p>");
+    });
+
+    // Advanced, encapsulation with closures
+    $("#closure-example-3").click(function () {
+        var output = $("#output");
+        output.html(""); // clear
+
+        var c = (function () {
+            var i = 1;
+            return function () {
+                i++;
+                return i;
+            };
+        })(); // trigger execution of closure
+        output.append("<p>" + c() + "</p>");
+        output.append("<p>" + c() + "</p>");
+        output.append("<p>" + c() + "</p>");
+        output.append("<p>" + typeof (i) + "</p>"); // i is undefined, only declared in the closure
+    });
+
+});
