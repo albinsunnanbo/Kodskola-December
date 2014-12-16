2c7d274f5ce9b31b4461a8608542e2884dc475d5
Albin Sunnanbo <albin@sunnanbo.com>
2014-12-16 20:37:48 +0100

    Add an example with foreach

---
+++ KodskolaDecember/Scripts/examples/foreach-example.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+    $("#foreach-example-setup").click(function () {
+        $("#output").html(
+            "<p>Row1</p>" +
+            "<p>Row2</p>" +
+            "<p>Row3</p>" +
+            "");
+    });
+
+    $("#foreach-example-execute").click(function () {
+        var output = '';
+
+        $("#output").find("p")
+            .each(function () {
+                output += ". ";
+            })
+            .each(function () { // Chaining: denna .each jobbar på samma lista av <p> som den förra
+                // this blir helt magiskt vart och ett av <p> som finns
+                // magin ligger i 
+                output += $(this).html() + " ";
+            });
+
+        $("#output").append(output);
+    });
+});
