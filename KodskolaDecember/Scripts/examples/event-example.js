9beb5de76ec7c757845047a28caed02953ee1484
Albin Sunnanbo <albin@sunnanbo.com>
2014-12-17 12:19:49 +0100

    Refactor: break up nested functions to named functions on top level

--- KodskolaDecember/Scripts/examples/event-example.js
+++ KodskolaDecember/Scripts/examples/event-example.js
@@ -4,12 +4,14 @@
         var button = $("<input type='button' value='Create new buttons' />");
         $("#output").append(button);
 
-        button.click(function () {
+        var onButton2Click = function () {
+            $(this).val($(this).val() + " #");
+        };
+        var createButtons = function () {
             var button2 = $("<input type='button' value='Click me' />");
             $("#output").append(button2);
-            button2.click(function () {
-                $(this).val($(this).val() + " #");
-            });
-        });
+            button2.click(onButton2Click);
+        };
+        button.click(createButtons);
     });
 });
